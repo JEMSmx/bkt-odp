7726e97f17b7415b0c8965cbd197cca7a679a663
Jesai Navarro <jesaa@Jesa.local>
2018-01-25 13:27:33 -0600

    Fix activity-extra

--- public/site/templates/add-extra-activity.php
+++ public/site/templates/add-extra-activity.php
@@ -4,7 +4,7 @@
 $ch = new Page();
 $ch->setOutputFormatting(false);
 $ch->template = 'activities'; 
-$ch->parent = wire('pages')->get(1643);
+$ch->parent = wire('pages')get('/actividades-extra');
 $ch->title=$input->post->title;
 $ch->duration=$input->post->duration;
 $ch->type='activity-extra';
